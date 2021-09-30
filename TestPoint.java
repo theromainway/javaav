54d3a4fced4a6dfb842e2177c5e3020f0a2cdcc6
theromainway <romaindiop64@gmail.com>
2021-09-30 14:47:27 +0200

    First try to upload to gitlab

---
+++ TestPoint.java
@@ -0,0 +1,15 @@
+package fr.dauphine.javaavance.td1;
+
+public class TestPoint {
+	private int x, y;
+	static void method() {
+		Point p=new Point();
+		System.out.println(p.x+" "+p.y);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
