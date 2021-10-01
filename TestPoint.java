a8456afa7fa8a15cc083d3baea48197161ca6aec
theromainway <romaindiop64@gmail.com>
2021-10-01 09:30:58 +0200

    correction des erreurs q4

--- TestPoint.java
+++ TestPoint.java
@@ -1,15 +1,12 @@
 package fr.dauphine.javaavance.td1;
 
 public class TestPoint {
-	private int x, y;
-	static void method() {
-		Point p=new Point();
-		System.out.println(p.x+" "+p.y);
-	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+		
+		Point p=new Point(2, 6);
+		System.out.println(p.getX()+" "+p.getY());
 	}
-
 }
